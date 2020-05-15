dcbdc77dbc70c2d9bd32a75c62f328fe50b40e41
Mike Rix Wolfe <michaelrixwolfe@gmail.com>
2020-05-14 22:39:37 -0500

    remove gallery

--- twiml_server/__init__.py
+++ twiml_server/__init__.py
@@ -9,8 +9,7 @@ db = SQLAlchemy(app)
 
 from twiml_server.resources.endpoint import Endpoint
 from twiml_server.resources.messages import Messages
-from twiml_server.resources.gallery import Gallery
 
 api.add_resource(Endpoint, '/twilioendpoint')
 api.add_resource(Messages, '/messages/<account_sid>/<phone_number>')
-api.add_resource(Gallery, '/gallery/<account_sid>/<phone_number>')
+
